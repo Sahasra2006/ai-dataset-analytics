0c43f2d731ccee6cdfe4cfa2cbbee4a0ef8c056c
Sahasra <sahasrashivamsh0926@gmail.com>
2026-06-03 16:49:30 +0530

    Update production API URL

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://ai-dataset-analytics.onrender.com/api',
 })
 
 api.interceptors.request.use((config) => {
